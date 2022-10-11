273d48ebe8c30b7d74a1e151a3f9b71ce496f373
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-10-11 13:04:49 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Picture from './components/Picture';
+import data from './helper/data';
+
+function App() {
+  return (
+    <div className="App row mx-auto d-flex justify-content-evenly gap-3 my-3">
+      <h1>Best Photos</h1>
+      {
+        data.map((item,a)=>
+        <Picture key={a} photographer={item.photographer} large = {item.src.large}/>
+        )
+      }
+    
+    </div>
+  );
+}
+
+export default App;
